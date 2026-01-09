de1666862d02d8abacf75730b07a1d3d93aba2a6
ammy m <murilloajimena@gmail.com>
2026-01-09 10:29:23 -0500

    Refactor coffee descriptions and DOM element retrieval

--- js/main.js
+++ js/main.js
@@ -1,29 +1,37 @@
+//Datos de las bebidas de temporada
+
 const seasonalCoffees = [
   {
     title: "Iced Caramel Latte",
     img: "https://i.pinimg.com/1200x/eb/b2/2e/ebb22ee771064a957e8c99bd559250bc.jpg",
-    desc: "El Iced Caramel Latte es una bebida fría a base de espresso, leche y jarabe de caramelo. Tiene un sabor suave y dulce, con el equilibrio perfecto entre la intensidad del café y la cremosidad de la leche. Es ideal para refrescarse en días calurosos y disfrutar de un toque dulce."
-    
+    desc:"Tiene un sabor suave y dulce, con el equilibrio perfecto entre la intensidad del café y la cremosidad de la leche. Es ideal para refrescarse en días calurosos y disfrutar de un toque dulce."
   },
   {
     title: "Vanilla Sweet Cream Cold Brew",
     img: "https://i.pinimg.com/1200x/19/8f/1a/198f1a08be08dcbd93619278962ffbd4.jpg",
-    desc: "El Vanilla Sweet Cream Cold Brew es una bebida fría elaborada con cold brew (café infusionado en frío por varias horas), lo que le da un sabor suave y menos ácido. Se mezcla con crema dulce de vainilla, que aporta un toque cremoso y dulce. Es una opción refrescante y deliciosa para los amantes del café que buscan algo diferente."
+    desc: "Se mezcla con crema dulce de vainilla, que aporta un toque cremoso y dulce. Es una opción refrescante y deliciosa para los amantes del café que buscan algo diferente."
   },
   {
     title: "Mocha Frappé",
     img: "https://coffeeclub.com.au/cdn/shop/files/Beverages_Product_Images_1200x1200_MochaFrappe_1080x.jpg?v=1716268834",
-    desc: "El Mocha Frappé es una bebida fría y cremosa preparada con café, leche, hielo y chocolate. Tiene un sabor intenso y dulce, donde se combina el amargor suave del café con la riqueza del cacao. Es perfecto para quienes disfrutan de una bebida indulgente y refrescante."
+    desc: "Tiene un sabor intenso y dulce, donde se combina el amargor suave del café con la riqueza del cacao. Es perfecto para quienes disfrutan de una bebida indulgente y refrescante."
   }
 ];
 
 let index = 0;
 
-const img = document.getElementById("tempImg");
-const title = document.getElementById("tempTitle");
-const desc = document.getElementById("tempDesc");
+document.addEventListener("DOMContentLoaded", () => {
+  const img = document.getElementById("tempImg");
+  const title = document.getElementById("tempTitle");
+  const desc = document.getElementById("tempDesc");
+});
+
 
 function renderCoffee() {
+  const img = document.getElementById("tempImg");
+  const title = document.getElementById("tempTitle");
+  const desc = document.getElementById("tempDesc");
+
   img.src = seasonalCoffees[index].img;
   title.textContent = seasonalCoffees[index].title;
   desc.textContent = seasonalCoffees[index].desc;
@@ -40,3 +48,4 @@ document.querySelector(".prev").onclick = () => {
 };
 
 renderCoffee();
+
